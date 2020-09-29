6ab340cbeb198e855597aa73548690d79ab5b3ae
Benjamin Wild <b.w@fu-berlin.de>
2020-09-29 13:01:46 +0200

    Training script improvements

--- temporal_nmf/scripts/train.py
+++ temporal_nmf/scripts/train.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
+
 import datetime
 import sys
 
 import click
 import pandas as pd
 import torch
+import tqdm.auto as tqdm
 
 from temporal_nmf.training import DataWrapper, TrainingWrapper
 
@@ -11,8 +14,9 @@ from temporal_nmf.training import DataWrapper, TrainingWrapper
 @click.command()
 @click.argument("base_path", type=click.Path(exists=True))
 @click.argument("output_path")
-@click.option("--batch_size", default=64)
-@click.option("--num_batches", default=200000)
+@click.option("--batch_size", default=256)
+@click.option("--device", default="cuda:0")
+@click.option("--num_batches", default=50000)
 @click.option("--num_hidden", default=32)
 @click.option("--num_embeddings", default=16)
 @click.option("--num_factors", default=8)
@@ -21,7 +25,7 @@ from temporal_nmf.training import DataWrapper, TrainingWrapper
 @click.option("--lambda_embedding_sparsity", default=0.1)
 @click.option("--lambda_factor_nonnegativity", default=1)
 @click.option("--lambda_adversarial", default=0.1)
-def train(base_path, output_path, batch_size, num_batches, **kwargs):
+def train(base_path, output_path, batch_size, device, num_batches, **kwargs):
     pd.set_option("display.float_format", lambda x: "%.4f" % x)
 
     lambdas = {
@@ -31,8 +35,6 @@ def train(base_path, output_path, batch_size, num_batches, **kwargs):
         "factor_nonnegativity": kwargs["lambda_factor_nonnegativity"],
         "adversarial": kwargs["lambda_adversarial"],
     }
-
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
     print(lambdas)
 
     data16 = DataWrapper(base_path, "bn16", datetime.datetime(2016, 7, 23))
@@ -40,11 +42,8 @@ def train(base_path, output_path, batch_size, num_batches, **kwargs):
 
     trainer = TrainingWrapper((data16, data19), device, lambdas)
 
-    for i in range(len(trainer.loss_hist), num_batches):
-        sys.stdout.write("\r{}/{} - {:.2f}".format(i, num_batches, trainer.batch(batch_size)))
-
-        if (i % 1000) == 0:
-            print("\n\n", pd.DataFrame(trainer.loss_hist[-1000:]).mean(axis=0))
+    for i in tqdm.trange(len(trainer.loss_hist), num_batches):
+        trainer.batch(batch_size)
 
     trainer.save(output_path)
 
